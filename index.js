7f556c1c6e880b8288994875f77771214832ae53
kabeer-necrox <khushkabeer@gmail.com>
2024-01-02 15:04:44 +0300

    here is the backend code

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var colors = require('colors');
+const express = require("express")
+require('./config')
+const item = require('./items')
+
+const app = express();
+
+app.use(express.json())
+app.post("/create",(req,res)=>{
+    console.log(req.body);
+    res.send("done")
+})
+app.listen(5000)
+
+
 console.log("here is the backend technology".underline.italic.bgYellow)
